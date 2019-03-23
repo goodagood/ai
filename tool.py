f3b856231f0b9b1c7e2bbfb46c676954bcc1ef44
andrew <bin1510@google.com>
2019-03-23 09:41:04 +0000

    0323 19 server 0941

--- tool.py
+++ tool.py
@@ -27,3 +27,22 @@ def show2ndP(text):
     else:
         print(sent_list[0])
 
+
+# c
+def readFirstLineOfInputFile(filename):
+    if not filename: return
+    with open(filename) as f :
+        r = f.read().strip()
+        l = r.split("\n")[0]
+        return l
+    return None
+
+import pydoc
+import sh
+def showLine(link):
+    if not link: return
+    dump = sh.w3m("-dump", link)
+    dump = str(dump)
+
+    pydoc.pager(dump)
+    pass
